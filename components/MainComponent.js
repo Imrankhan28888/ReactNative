e617024f2a2a10fe2ec8898dd0035c78c0848504
Imran <imran_khan2888@hotmail.com>
2020-07-22 06:58:50 -0500

    Week4- Secure Store - Login option, Notifications

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -12,6 +12,7 @@ import { connect } from 'react-redux';
 import { fetchCampsites, fetchComments, fetchPromotions, fetchPartners } from '../redux/ActionCreators';
 import Reservation from './ReservationComponent';
 import Favorites from './FavoritesComponent';
+import Login from './LoginComponent';
 
 const mapDispatchToProps = {
     fetchCampsites,
@@ -186,8 +187,46 @@ const CustomDrawerContentComponent = props => (
     </ScrollView>
 );
 
+
+const LoginNavigator = createStackNavigator(
+    {
+        Login: { screen: Login }
+    },
+    {
+        navigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='sign-in'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
+
 const MainNavigator = createDrawerNavigator(
     {
+        Login: {
+            screen: LoginNavigator,
+            navigationOptions: {
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='sign-in'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
+        
         Home: {
             screen: HomeNavigator,
             navigationOptions: {
@@ -278,6 +317,7 @@ const MainNavigator = createDrawerNavigator(
         
     },
     {
+        initialRouteName: 'Home',
         drawerBackgroundColor: '#CEC8FF',
         contentComponent: CustomDrawerContentComponent
     }
